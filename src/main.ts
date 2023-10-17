cae5b800c13e14767474fb4c66aafd92929fddf8
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-17 05:19:53 -0400

    Quick fixes for new APIs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { useAtomicApi } from '@atomicdesign/atomic-singularity';
-import { VueGovernor } from '@atomicdesign/atomic-vue';
+import { useAtomicVue } from '@atomicdesign/atomic-vue';
 //import { OriginGovernor } from '@atomicdesign/atomic-origin';
 
 import { HaloCMSSDKDefaultModules } from '@golden-circuit-technologies/halocms-sdk';
 
 useAtomicApi()
-  .useGovernor(VueGovernor)
-  //.useGovernor(OriginGovernor)
-  .useModule(HaloCMSSDKDefaultModules)
+  .use(useAtomicVue())
+  .use(HaloCMSSDKDefaultModules)
   .start()
