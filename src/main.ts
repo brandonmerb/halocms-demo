474e49f63b73d30af3314d32d5b9dbc9980b421e
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-20 00:25:34 -0400

    Nebula rename

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { useAtomicApi } from '@atomicdesign/atomic-singularity';
 import { useAtomicVue } from '@atomicdesign/atomic-vue';
-//import { OriginGovernor } from '@atomicdesign/atomic-origin';
+//import { OriginNebula } from '@atomicdesign/atomic-origin';
 
 import { HaloCMSSDKDefaultModules } from '@golden-circuit-technologies/halocms-sdk';
 
