275de2cf0c8c3013828897c014bd6e6c2cf2f756
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-20 20:55:59 -0400

    DI testing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import "reflect-metadata";
+
 import { useAtomicApi } from '@atomicdesign/atomic-singularity';
 import { useAtomicVue } from '@atomicdesign/atomic-vue';
+import { useAtomicSDK } from "@atomicdesign/atomic-sdk";
+import { useTSyringe } from "@atomicdesign/atomic-singularity-tsyringe-wrapper";
 //import { OriginNebula } from '@atomicdesign/atomic-origin';
 
 import { HaloCMSSDKDefaultModules } from '@golden-circuit-technologies/halocms-sdk';
-
 useAtomicApi()
   .use(useAtomicVue())
+  .use(useTSyringe)
+  .use(useAtomicSDK)
   .use(HaloCMSSDKDefaultModules)
   .start()
